4ba760eeac23bb17764f3bedd4747a0dabbae177
lujine-abdelfattah <lujine_2005@yahoo.com>
2021-06-02 10:38:13 -0400

    testerlulu

---
+++ tester.java
@@ -0,0 +1,8 @@
+//tester for our Bynder app
+public class tester{
+    public static void main(String[] args) {
+        System.out.println("Claire is a loser.");
+        System.out.println("A really hot loser.");
+        //this is very school appropriate!
+    }
+}
