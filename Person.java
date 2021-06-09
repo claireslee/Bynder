b8dcc726ac19a223edd192a978e6275773caa47f
Claire <clairebearlee0126@gmail.com>
2021-06-09 12:12:53 -0400

    stored data into separate arrays (organize values)

---
+++ Person.java
@@ -0,0 +1,11 @@
+package Bynder;
+
+public class Person extends ReadURL {
+
+    public Person(String i) {
+        String per = i.substring(18, i.length());
+
+    }
+
+    
+}
--- ReadURL.java
+++ ReadURL.java
@@ -1,13 +1,47 @@
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
+
 public class ReadURL {
     public static void main(String[] args) throws Exception {
-        URL url = new URL("https://docs.google.com/spreadsheets/d/e/2PACX-1vSDoMI2QjH3SOGisSBy5Hgbe90_MN0L_R4AxRtgUdpup04WAEfwP1LarC20StZ369BGkPqKkky6hvkv/pub?gid=376584242&single=true&output=tsv");
+        URL url = new URL("https://docs.google.com/spreadsheets/d/e/2PACX-1vQOs65MyglyYnZOy9B9KyYGd9bjKOAMo63bMDzxFQ6q_JLknmhBtLz-N0XBshC4z6f0QNSRneXFxaeN/pub?gid=321494671&single=true&output=tsv");
         BufferedReader read = new BufferedReader(
         new InputStreamReader(url.openStream()));
+        
+        String arr[] = new String[1000];
+        
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> age = new ArrayList<>();
+        ArrayList<String> hobbies1 = new ArrayList<>();
+        ArrayList<String> hobbies2 = new ArrayList<>();
+        ArrayList<String> movie = new ArrayList<>();
+        ArrayList<String> insideChar = new ArrayList<>();
+        ArrayList<String> mbti = new ArrayList<>();
+        ArrayList<String> island = new ArrayList<>();
+
         String i;
-        while ((i = read.readLine()) != null)
-            System.out.println(i);
+        read.readLine();
+        while ((i = read.readLine()) != null) {
+            arr = i.split("\t");
+            name.add(arr[1]);
+            age.add(arr[2]);
+            hobbies1.add(arr[3]);
+            hobbies2.add(arr[4]);
+            movie.add(arr[5]);
+            insideChar.add(arr[6]);
+            mbti.add(arr[7]);
+            island.add(arr[8]);
+        }
+
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(hobbies1);
+        // System.out.println(hobbies2);
+        // System.out.println(movie);
+        // System.out.println(insideChar);
+        // System.out.println(mbti);
+        // System.out.println(island);
+
         read.close();
     }
 }
